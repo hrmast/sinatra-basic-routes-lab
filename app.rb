9451c2fdf0678d24abbd86281a8a9f0677b1d2c1
Heath Mast <hrmast@gmail.com>
2021-12-02 16:54:25 -0600

    commit tree 5166066ab449574655915358a11de349bbdd4c83

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Heath"
+    end
+
+    get '/hometown' do
+        "My hometown is Benton, OH."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is IDK"
+    end
+
 end
